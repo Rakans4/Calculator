d18a4adc5dc8fc121c6f66dad8ee378787dd56ef
Rakan <RakanQ5@outlook.com>
2022-02-22 22:26:50 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { FieldComponent } from './field/field.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    FieldComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
