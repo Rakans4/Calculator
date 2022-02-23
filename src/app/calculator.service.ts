0d888cdb34b75ca01e640e1ebbdc6fe5312c364c
Rakan <RakanQ5@outlook.com>
2022-02-23 14:03:42 +0300

    calc layout

--- src/app/calculator.service.ts
+++ src/app/calculator.service.ts
@@ -4,6 +4,43 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CalculatorService {
+  total: number = 0;
+  floatNumber: boolean = false;
 
   constructor() { }
+
+  add(a:number, b:number): number {
+    this.floatNumber = false;
+    return a+b;
+  }
+
+  subtract(a:number, b:number): number {
+    this.floatNumber = false;
+    return a-b;
+  }
+
+  multiply(a:number, b:number): number {
+    this.floatNumber = false;
+    return a*b
+  }
+
+  // handle divide by zero
+  divide(a:number, b:number): number {
+    this.floatNumber = false;
+    return a/b;
+  }
+
+  typeFloatNumber():boolean {
+    this.floatNumber = true;
+    return this.floatNumber;
+  }
+
+  decimals(a:number, b:number): number {
+    let floats = 1;
+    for(let i = 1; i <= b.toString().length; i++){
+      floats*10
+    }
+    b = b/floats;
+    return Math.floor(a)+b;
+  }
 }
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
-
+  theme: number = 1;
+  buttons: string[] = [
+    '7', '8', '9', 'DEL','4', '5', '6', '+', '1', '2', '3', '-', '.', '0', '/', 'x', 'RESET', '='
+  ]
   constructor() { }
 
   ngOnInit(): void {
